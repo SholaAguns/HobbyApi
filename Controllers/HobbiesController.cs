a1c312009b6ef53e79714dbc206b0f6cb037e044
shola.agunbiade <shola.agunbiade@youlend.com>
2024-06-13 14:22:47 +0100

    Added get categories and filter by category

--- Controllers/HobbiesController.cs
+++ Controllers/HobbiesController.cs
@@ -23,9 +23,24 @@ namespace HobbyApi.Controllers
 
         // GET: api/hobbies
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Hobby>>> GetHobbies()
+        public async Task<ActionResult<IEnumerable<Hobby>>> GetHobbies(string? category)
         {
-            return await this.context.Hobbies.ToListAsync();
+            if(string.IsNullOrWhiteSpace(category))
+            {
+                return await this.context.Hobbies.ToListAsync();
+            }
+
+            category = category.Trim();
+            return await this.context.Hobbies.Where(c=> c.Category == category).ToListAsync();
+        }
+
+        // GET: api/categories
+        [HttpGet("categories")]
+        public async Task<ActionResult<IEnumerable<Hobby>>> GetCategories()
+        {
+            var categories = await this.context.Hobbies.Select(h => h.Category).Distinct().ToListAsync();
+
+            return Ok(categories);
         }
 
         // POST: api/hobbies
